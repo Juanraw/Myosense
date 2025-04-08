87783ff836d2e63445ccbee6c1cd89fd17c5ff09
Juan David <est.juan.ramirez3@unimilitar.edu.co>
2025-04-08 18:58:53 -0500

    Myo functions

--- src/myoread.cpp
+++ src/myoread.cpp
@@ -1,5 +1,46 @@
 #include "myoread.hpp"
 
 MyoRead::MyoRead(QObject *parent)
-    : QObject{parent}
+
+    : QObject{parent}, running(false), hub(nullptr), myo(nullptr)
 {}
+
+MyoRead::~MyoRead() {
+
+    stop();
+
+}
+
+void MyoRead::run() {
+
+    hub = new myo::Hub("com.myosense.qt.myo");
+    myo = hub->waitForMyo(10000);
+    if (!myo) return;
+
+    myo->setStreamEmg(myo::Myo::streamEmgEnabled);
+    hub->addListener(this);
+
+    running = true;
+    while(running) {
+        hub->run(1000 / 50);
+    }
+
+}
+
+void MyoRead::stop() {
+
+    running = false;
+    //wait();
+    delete hub;
+
+}
+
+void MyoRead::onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg) {
+
+    QVector<qint8> data(8);
+    for (int i = 0; i < 8; ++i) {
+        data[i] = emg[i];
+    }
+    emit emgDataReceived(static_cast<qint64>(timestamp), data);
+
+}
--- src/myoread.hpp
+++ src/myoread.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifndef MYOREAD_HPP
 #define MYOREAD_HPP
 
@@ -6,13 +8,28 @@
 #include <array>
 #include <myo/myo.hpp>
 
-class MyoRead : public QObject
+class MyoRead : public QObject, public myo::DeviceListener
 {
     Q_OBJECT
 public:
     explicit MyoRead(QObject *parent = nullptr);
+    ~MyoRead();
+    void run();
+    void stop();
 
 signals:
+
+    void emgDataReceived(quint64 timestamp, QVector<qint8> emg);
+
+protected:
+
+    void onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg) override;
+
+private:
+    bool running;
+    myo::Hub* hub;
+    myo::Myo* myo;
+
 };
 
 #endif // MYOREAD_HPP
